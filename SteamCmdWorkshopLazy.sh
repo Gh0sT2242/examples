af89ea5943294ac8bb3a56ce473c2f92b46ea69d
Gh0sT2242 <34068333+Gh0sT2242@users.noreply.github.com>
2022-04-29 15:39:27 -0500

    Add my Lazy steamcmd script

---
+++ SteamCmdWorkshopLazy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+steamcmd +login [REDACTED] [REDACTED] +workshop_download_item 107410450814997 +workshop_download_item 107410 463939057 +workshop_download_item 107410 843577117 +worksho$
